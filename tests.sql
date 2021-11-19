01fd9f362d436e23d24f78266a57ea5c783bb210
HAL <43222922+halBRY@users.noreply.github.com>
2021-11-18 21:46:18 -0600

    Update tests.sql

--- tests.sql
+++ tests.sql
@@ -3,6 +3,10 @@ INSERT INTO SalesAssoc (name,passwd,accumComm,address) VALUES('Mary Beth','pass1
 
 INSERT INTO Quote VALUES(1,1,0,CURRENT_TIME,1.4,'Secret Note test.', 149,'cool@yahoo.com');
 INSERT INTO Quote (salesAID,status,procDateTime,commission,sNotes,customerID,cusContact) VALUES(2,4,CURRENT_TIME,1.5,'Secret Note test part 2.',20,'swag@gmail.com');
+INSERT INTO Quote (salesAID,status,procDateTime,commission,sNotes,customerID,cusContact) VALUES(1,0,CURRENT_TIME,4.20,'Cool fun secret note!',23,'test@email.com);
+INSERT INTO Quote (salesAID,status,procDateTime,commission,sNotes,customerID,cusContact) VALUES(2,1,CURRENT_TIME,333.333,'Another secret note. Give this person no discounts',1,'mail@mail.net);
+INSERT INTO Quote (salesAID,status,procDateTime,commission,sNotes,customerID,cusContact) VALUES(1,2,CURRENT_TIME,2,'Hi',2,'2@2.org');
+
 
 INSERT INTO LineItem VALUES(1,1,'This is a Line Item.',45.45);
 INSERT INTO LineItem (quoteID,description,price) VALUES(2,'Line Item 2',5000.00);
