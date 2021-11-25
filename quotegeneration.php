c7505222a223c85d694d798431ef16e28aedefd2
ojsi <ojrajmane@gmail.com>
2021-11-25 17:48:38 -0600

    tried to add database connection

--- quotegeneration.php
+++ quotegeneration.php
@@ -1,7 +1,21 @@
+<?php
+include("credentials.php");	// Store your $username and $password in this file
+include("dbLoginFunc.php");
+include("login.php")
+?>
 <!DOCTYPE html>
 <html>
-    <!--Validate Input Function-->
+<!--Validate Input Function-->
 <?php
+
+$pdoQuoteDB = loginToDatabase("courses", $username, $username, $password);
+
+// connect to external customer info legacy database
+$pdoCustDB = loginToDatabase("blitz.cs.niu.edu", "csci467", "student", "student");    
+$legacyPDO = login_to_database(args);
+?>
+
+<?php 
     function test_input($data)
     {
         $data = trim($data);
@@ -20,7 +34,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $sql = "SELECT * FROM customers WHERE name='$customerSelection'";
         $customer = $legacyPDO->query($sql);
         $info = $customer->fetchAll(PDO::FETCH_ASSOC);
-        //if the post request isn't empty gather the data from it
         if (!empty($info)) {
             $name = $info[0]["name"];
             $street = $info[0]["street"];
@@ -43,13 +56,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>  
     <div id="CustomerSelection">
         <div class="p-1 btn-group d-flex">
-            <a href="CustomerList.php" class="btn btn-primary" role="button" id="CustomerSelect" target="_self">Select Customer Info For Quote</a>
+            <a href="CustomerAccess.php" class="btn btn-primary" role="button" id="CustomerSelect" target="_self">Select Customer Info For Quote</a>
         </div>
     </div>
-    <form id="QuoteForm" action="confirm.php" class="border border-primary rounded mx-1 px-2" method="post">
+    <form id="QuoteForm" action="QuoteValidation.php" class="border border-primary rounded mx-1 px-2" method="post">
         <fieldset>
             <h4 class=".bg-info">Create New Quote</h4>
-            <!-- Customer Information form group, All data is imported from Legacy database -->
+            <!-- Customer Information form group, All of the data is imported from Legacy database -->
             <div class="form-group">
                 <div class="row">
                     <div class="col">
@@ -61,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <label for="contact">Contact</label>
                         <input onkeydown="event.preventDefault()" class="form-control" type="text" value="<?php echo $contact ?>" name="contact" id="contact" placeholder="Contact"><br>
                     </div>
-                </div>
+                 </div>
                     <div class="row">
                     <div class="col">
                         <label for="email">Email</label>
@@ -78,3 +91,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                 </div>
             </div>
+
+            <div id="QuoteContent">
+                <div class="btn-group d-flex">
+                    <button type="button" id="addField" class="btn btn-primary m-1">Add New Field+</button>
+                </div>
+            </div>
+
+            <!-- Contains any secret Notes for the Quote -->
+            <textarea name="message" class="form-control mt-2" placeholder="Secret Notes" rows="5"></textarea><br>
+            <div class="btn-group d-flex">
+                <button class="btn btn-success m-1" type="submit">Submit Quote</button>
+            </div>
+
+        </fieldset>
+    </form>
+    <!-- Scripts used for the addition of line items -->
+    <script type="text/javascript" src="javascript/quotegen.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.3/es6-shim.min.js"></script>
+</body>
+
+</html>
